46d73595a2c81763bd915f5f2fe43b93e3af29c3
Zemiek <mikael.holmer1@gmail.com>
2018-03-27 10:20:06 +0200

    Some changes

--- django/reactjs/containers/App1Container.jsx
+++ django/reactjs/containers/App1Container.jsx
@@ -1,14 +1,29 @@
 import React from "react"
-
+import Radium from "radium"
 import Headline from "../components/Headline"
 
+
+const styles = {
+  button: {
+    cursor: "pointer",
+  },
+  counter: {
+    color: "blue",
+    fontSize: "20px",
+  }
+}
+
+
+@Radium
 export default class App1Container extends React.Component {
+
   render() {
     return (
-      <div className="container">
+<div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Headline>IT WORKS!</Headline>
+            <Headline>Sample App!</Headline>
+            <button style={[styles.button]}>Hej</button>
           </div>
         </div>
       </div>
