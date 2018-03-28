73d2b758ea2663c64533e98b146f054f67b853ab
Fruktfisk <Fruktfisk@github.com>
2018-03-28 08:55:22 +0200

    katt

--- django-unchained/mysite/mysite/settings.py
+++ django-unchained/mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from secretkey import *
+from mysite.secretkey import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,8 +73,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'locus',
+        'USER': 'fruktfisk',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
