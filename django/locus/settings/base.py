774e896c59adff11f13cf70da70ecfc28fc9a64d
Zemiek <mikael.holmer1@gmail.com>
2018-04-03 14:19:03 +0200

    added working login where you have to login to make review

--- django/locus/settings/base.py
+++ django/locus/settings/base.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'locus.urls'
+LOGIN_URL = '/login/'
 
 TEMPLATES = [
     {
--- django/locus/urls.py
+++ django/locus/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 from django.views import generic
 from reviews.views import about_view, ReviewsListView, ReviewsDetailView, ReviewCreateView
 
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', LoginView.as_view(), name = 'login'),
     url(r'^$', generic.TemplateView.as_view(template_name='home.html')),
     url(r'^reviews/$', ReviewsListView.as_view()),
     url(r'^reviews/create/$', ReviewCreateView.as_view()),
@@ -15,18 +18,3 @@ urlpatterns = [
 
 ]
 
-"""
-from django.conf.urls import url
-from django.contrib import admin
-from django.views import generic
-from .views import review
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^review/',
-        generic.TemplateView.as_view(template_name='review.html')),
-    url(r'^$',
-        generic.TemplateView.as_view(template_name='homepage.html')),
-    url(r'^review/$', review.make_review, name='make_review'),
-]
->>>>>>> a88cf7ceac347260db1247d0bb7752f14cae63a6:django/djreact/urls.py
